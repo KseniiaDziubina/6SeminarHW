61f543e64ea6a8ae373230a27d9f6c349dca3ac6
Kseniia <ksenjadzjubina@gmail.com>
2022-07-28 21:26:04 +0300

    Задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите числа через пробел  ");
+
+int[] myArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+int count = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+{
+       if (myArray[i]> 0)
+       {
+           count++;
+       }
+}
+Console.WriteLine($"Количество введённых чисел больше 0: {count}");

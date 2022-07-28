352656da9a628293012f3435f10692feef6ddd01
Kseniia <ksenjadzjubina@gmail.com>
2022-07-28 21:32:27 +0300

    Задача 43

---
+++ Task43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите коэффицент k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффицент b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффицент k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффицент b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+var x = (b2 - b1) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+ 
+Console.WriteLine($"Пересечение прямых в точке с координатами ({x}; {y})");
